f3ac109c2962532b76dcdfb83f57af257380119f
nhu <tranthenhu150@gmail.com>
2023-01-10 13:39:57 +0700

    change app name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   head: {
-    title: 'nuxt-demo',
+    title: 'Naeding',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
